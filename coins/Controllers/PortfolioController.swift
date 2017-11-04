321f6089f90446044befc400373fa0144340f41d
Jeffrey Huang <jeffreyrhuang@gmail.com>
2017-11-03 23:05:15 -0600

    add ticker api call

--- coins/Controllers/PortfolioController.swift
+++ coins/Controllers/PortfolioController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class PortfolioController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let TICKER_API = "https://api.coinmarketcap.com/v1/ticker/"
 
     @IBOutlet weak var coinHoldingTable: UITableView!
     @IBOutlet weak var totalLabel: UILabel!
@@ -18,12 +22,30 @@ class PortfolioController: UIViewController, UITableViewDataSource, UITableViewD
         
         coinHoldingTable.dataSource = self
         coinHoldingTable.delegate = self
+        
+        getTickerData(url: TICKER_API, parameters: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: Networking
+    
+    func getTickerData(url: String, parameters: [String:String]?) {
+        
+        Alamofire.request(url, method: .get).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Successful call to API")
+            } else {
+                print("Connection Issues")
+            }
+        }
+    }
+    
+    // MARK: Table functions
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "CoinHoldingCell") as? CoinHoldingCell {
@@ -43,6 +65,8 @@ class PortfolioController: UIViewController, UITableViewDataSource, UITableViewD
         let coinHolding = DataService.instance.getCoinHoldings()[indexPath.row]
         performSegue(withIdentifier: "CoinDetailsController", sender: coinHolding)
     }
+    
+    // MARK: Segues
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let coinDetailsController = segue.destination as? CoinDetailsController {
